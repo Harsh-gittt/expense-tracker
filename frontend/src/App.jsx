46af5b6d2713d7a465e3c446295c630ecabbfaef
vishujudiyan2005-debug <vishujudiyan2005@gmail.com>
2026-06-22 15:13:47 +0530

    signup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,14 @@
 import React from 'react'
-import Navbar from './components/Navbar'
-import Hero from './components/HeroPage1'
+import Page1 from './components/Page1'
+import Card from './components/Card'
+import Signup from './components/Signup'
+
 
 const App = () => {
   return (
-    <div className="flex flex-col h-screen bg-white overflow-hidden">
-      <Navbar />
-      <main className="flex flex-1 overflow-hidden">
-        <Hero />
-      </main>
-    </div>
+   <div>
+    <Signup/>
+   </div>
   )
 }
 
---
+++ frontend/src/components/Signup.jsx
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+
+const Signup = () => {
+  const [userName, setUserName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPass] = useState("");
+const submitHandler= (e)=>{
+    e.preventDefault();
+    
+}
+
+  return (
+    <div className="flex h-screen w-screen justify-center items-center">
+      <form className=" shadow-2xl flex flex-col  h-90 w-120 justify-center items-center px-8 gap-6 relative rounded-3xl">
+
+        <input
+          onChange={(e) => setUserName(e.target.value)}
+          className="w-full bg-gray-200 py-2 text-l px-2 font rounded-md"
+          type="text"
+          placeholder="Name:"
+        />
+
+        <input
+          onChange={(e) => setEmail(e.target.value)}
+          className="w-full bg-gray-200 py-2 text-l px-2 font rounded-md"
+          type="text"
+          placeholder="Email:"
+        />
+        
+        <input
+          onChange={(e) => setPass(e.target.value)}
+          className="w-full bg-gray-200 py-2 text-l px-2 font rounded-md"
+          type="password"
+          placeholder="Password:"
+        />
+        <button
+        onClick={submitHandler}
+        
+         className="mt-7 bottom-3 w-full border-none rounded-xl bg-blue-600 py-3 text-xl text-white font-bold cursor-pointer transition-all duration-300 hover:scale-105">
+          Sign up
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default Signup;
