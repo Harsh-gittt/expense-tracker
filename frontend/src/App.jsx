15b8c29c256dab4aeaa975ca208cf44413e6080f
vishujudiyan2005-debug <vishujudiyan2005@gmail.com>
2026-06-22 16:19:24 +0530

    signUP backend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Page1 from './components/Page1'
 import Card from './components/Card'
 import Signup from './components/Signup'
+import SignIn from './components/SignIn'
 
 
 const App = () => {
   return (
    <div>
-    <Signup/>
+  <Page1/>
+  <SignIn/>
    </div>
   )
 }
---
+++ frontend/src/components/SignIn.jsx
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import axios from "axios";
+const SignIn = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPass] = useState("");
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    const response = await axios.post("http://localhost:3000/user/signin",{
+        "email": email,
+        "password": password
+      
+    })
+    console.log(response.data)
+    localStorage.setItem("token",response.data.token)
+  };
+
+  return (
+    <div className="flex h-screen w-screen justify-center items-center">
+      <form className=" shadow-2xl flex flex-col  h-90 w-120 justify-center items-center px-8 gap-6 relative rounded-3xl">
+      
+
+        <input
+          onChange={(e) => setEmail(e.target.value)}
+          className="w-full bg-gray-200 py-2 text-l px-2 font rounded-md"
+          type="text"
+          placeholder="Email:"
+        />
+
+        <input
+          onChange={(e) => setPass(e.target.value)}
+          className="w-full bg-gray-200 py-2 text-l px-2 font rounded-md"
+          type="password"
+          placeholder="Password:"
+        />
+        <button
+          onClick={submitHandler}
+          className="mt-7 bottom-3 w-full border-none rounded-xl bg-blue-600 py-3 text-xl text-white font-bold cursor-pointer transition-all duration-300 hover:scale-105"
+        >
+          Sign in
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default SignIn;
